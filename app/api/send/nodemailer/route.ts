f6aa8633f45e90d9aa0a746f5b5c60b31cec4539
LanaMaou <maulanarasyit777@gmail.com>
2024-06-01 13:14:01 +0800

    menambahkan cors di api

--- app/api/send/nodemailer/route.ts
+++ app/api/send/nodemailer/route.ts
@@ -6,6 +6,20 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const { email, userFirstname } = await req.json();
 
+  const res = NextResponse.json({ success: true }, { status: 200 });
+
+  res.headers.set("Access-Control-Allow-Origin", "*"); // Sesuaikan dengan asal yang diizinkan
+  res.headers.set("Access-Control-Allow-Methods", "POST"); // Metode yang diizinkan
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  ); // Header yang diizinkan
+
+  // Tangani preflight request
+  if (req.method === "OPTIONS") {
+    return res;
+  }
+
   try {
     await sendMail(
       email,
